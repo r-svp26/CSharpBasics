4903faabcfd03403852d169f90d34b73337fdd43
r-svp26 <riteshk6341@gmail.com>
2021-07-23 14:14:22 +0530

    [Ritesh] Add: Flip Coin Problem

---
+++ ProgrammingConstruct/FlipCoin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingConstruct
+{
+    class FlipCoin
+    {
+        // constants 
+        const int NUMBER_OF_TIMES_FLIP = 100;
+        const double FLAG = 0.5;
+        // variables
+        double headCount = 0, tailCount = 0;
+        double flip, headPercentage, tailPercentage;
+        Random random = new Random();
+        /// <summary>
+        /// evaluate head and tail percentage.
+        /// </summary>
+        public void GetPercentage()
+        {
+            for (int i = 1; i <= NUMBER_OF_TIMES_FLIP; i++)
+            {
+                flip = random.NextDouble();
+                if (flip < FLAG)
+                    tailCount++;
+                else
+                    headCount++;
+            }
+            tailPercentage = (tailCount / NUMBER_OF_TIMES_FLIP) * 100;
+            headPercentage = (headCount / NUMBER_OF_TIMES_FLIP) * 100;
+            Console.WriteLine("Tail percentage:" + tailPercentage + "%");
+            Console.WriteLine("Head percentage:" + headPercentage + "%");
+        }
+    }
+}
--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -4,9 +4,32 @@ namespace ProgrammingConstruct
 {
     class Program
     {
+        // variables
+        int option= Convert.ToInt32(Console.ReadLine());
+
+        /// <summary>
+        /// This method is used to call a specific class in the project.
+        /// </summary>
+        public void InvokeClass()
+            {
+                switch (option)
+                {
+                case 1:
+                    {
+                        FlipCoin flip = new FlipCoin();
+                        flip.GetPercentage();
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Please! Enter the valid option.");
+                    break;
+                }
+            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C# Basics Programming.");
+            Console.WriteLine("Enter the option for Class.");
+            Program program = new Program();
+            program.InvokeClass();
         }
     }
 }
