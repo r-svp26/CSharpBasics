f5d66012efab46997a9ac1a9477e4933461c6851
r-svp26 <riteshk6341@gmail.com>
2021-07-23 13:51:48 +0530

    [Ritesh] Add: display welcome message

---
+++ ProgrammingConstruct/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammingConstruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Basics Programming.");
+        }
+    }
+}
