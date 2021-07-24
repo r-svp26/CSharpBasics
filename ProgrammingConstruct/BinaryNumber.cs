f9049396fb03d060dcbb74e51d83fb7d43f9ddd9
r-svp26 <riteshk6341@gmail.com>
2021-07-24 05:56:02 +0530

    [Ritesh] Add: Binary Number

---
+++ ProgrammingConstruct/BinaryNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingConstruct
+{
+    class BinaryNumber
+    {
+        // variables 
+        static int decimalValue, remainder;
+        static string binary;
+        static BinaryNumber()
+        {
+            Console.WriteLine("Enter the Decimal Value");
+            decimalValue= Convert.ToInt32(Console.ReadLine());
+        }
+        /// <summary>
+        /// convert deicmal value to binary
+        /// </summary>
+        public static void ToBinary()
+        {
+            do
+            {
+                remainder = decimalValue % 2;
+                binary = remainder + binary;
+                decimalValue /= 2;
+            } while (decimalValue != 0);
+            Console.WriteLine("Binary Number:" +binary);
+        }
+    }
+}
